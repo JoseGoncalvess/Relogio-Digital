8a09bf0f6735037bbd5768e5ac270727e1f8b2d1
José Gonçalves <joseinacio774@gmail.com>
2022-09-02 17:58:00 -0300

    ajuste

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ const relogio = setInterval(function time() {
   let h = datatoday.getHours()
   let m = datatoday.getMinutes()
   let s = datatoday.getSeconds()
-  h = 2
   if (h < 10) h = '0' + h
   if (m < 10) h = '0' + m
   if (s < 10) h = '0' + s
@@ -15,3 +14,10 @@ const relogio = setInterval(function time() {
   minuto.textContent = m
   segundos.textContent = s
 })
+
+/*
+window.addEventListener('click', function(){
+  const audio  = document.getElementById('music');
+  audio.play();
+  audio.volume = 0.2  })
+*/
