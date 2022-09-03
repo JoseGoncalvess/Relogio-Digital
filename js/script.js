a711681c4566843071e5d9e6ee2ffb2c5a9e6f48
José Gonçalves <joseinacio774@gmail.com>
2022-09-03 12:02:02 -0300

    ajuste time secunds

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
 let hora = document.getElementById('horas')
 let minuto = document.getElementById('minutos')
 let segundos = document.getElementById('segundos')
+let milis = document.getElementById('mili')
 
-var sec = 00
+var mili = '00'
+var sec = '00'
 var min = '00'
-var hr = 00
+var hr = '00'
 var pausa
 var stoped
 
 function start() {
   //setinterval é uma funcion que chama a outro baseada no tempo que se coloca no 2 parametro
   time()
-  pausa = setInterval(time, 1)
+  pausa = setInterval(time, 10)
 }
 function paused() {
   clearInterval(pausa)
 }
 function stop() {
   clearInterval(pausa)
+  milis.innerText = '00'
   segundos.innerText = '00'
   minuto.innerText = '00'
   hora.innerText = '00'
 }
 
 function time() {
-  sec++
-
-  if (sec == 60) {
+  mili++
+  if (mili == 60) {
+    sec++
+    mili = 00
+  } else if (sec == 60) {
     min++
-    if (min < 10) {
-      min = '0' + min
-    }
     sec = 00
   } else if (min == 60) {
     hr++
-    if (hr < 10) {
-      hr = '0' + hr
-    }
     min = 00
   }
+  milis.innerText = mili
   segundos.innerText = sec
   minuto.innerText = min
   hora.innerText = hr
